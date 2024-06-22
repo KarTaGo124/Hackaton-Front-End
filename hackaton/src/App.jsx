e3a9461a3e21d5dab231e38e3f4a54895e0dea0e
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-06-21 22:49:03 -0500

    Set up

---
+++ hackaton/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import './App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/auth/login"/>} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register"element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />}/>
+        <Route path="*" element={<div>Not Found</div>} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
