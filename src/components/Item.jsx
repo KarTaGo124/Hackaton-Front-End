12e2d3ff9ba565662f5b1d99fe72867cbef4cb19
AlejandroEN <alejandroescobarnunez@gmail.com>
2024-06-22 17:33:21 -0500

    Refactor Item component for better readability and add purchase functionality

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,32 +1,37 @@
-import {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { deleteProduct } from '../services/api';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteProduct } from "../services/api";
 
-const Item = ({data}) => {
-  const navigate = useNavigate();
-  const [id, setId] = useState('');
+const Item = ({ data }) => {
+	const navigate = useNavigate();
+	const [id, setId] = useState("");
 
-  const handleDelete = async () => {
-    try {
-        setId(localStorage.getItem('itemId'));
-        await deleteProduct(id)
-        console.log('Deleted');
-        navigate('/dashboard');
-    } catch (error) {
-        console.log(error);
-    }
-}
-  return (
-    <div>
-      {Object.keys(data).map(key => (
-        <p key={key}>
-          <strong>{key}:</strong> {data[key]}
-        </p>
-      ))}
-      <button onClick={()=> handleDelete}>Delete Item</button>
-			<button onClick={() => }
-    </div>
-  );
-}
+	const handleDelete = async () => {
+		try {
+			setId(localStorage.getItem("itemId"));
+			await deleteProduct(id);
+			console.log("Deleted");
+			navigate("/dashboard");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	function handlePurchase() {
+		
+	}
+
+	return (
+		<div>
+			{Object.keys(data).map((key) => (
+				<p key={key}>
+					<strong>{key}:</strong> {data[key]}
+				</p>
+			))}
+			<button onClick={() => handleDelete}>Delete Item</button>
+			<button onClick={() => handlePurchase}>Comprar</button>
+		</div>
+	);
+};
 
 export default Item;
